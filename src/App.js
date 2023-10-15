fa5e00fa9036f3421a5a854201dbcb1ab45e085f
Raaj1500s <mr.k.manikanta99@gmail.com>
2023-10-15 11:52:39 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import Calculator from './components/Calc';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Calculator/>
+    </div>
+  );
+}
+
+export default App;
+//https://github.com/Raaj1500s/ReactJSTask-2.git
+/*
+echo "# ReactTask2" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Raaj1500s/ReactTask2.git
+git push -u origin main
+*/
